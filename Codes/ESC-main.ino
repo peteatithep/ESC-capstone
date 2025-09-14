e56d426f286f9f5796e0782f2268b6e62d7851f3
peteatithep <pete62980@hotmail.com>
2025-09-14 18:26:31 +0700

    Update ESC-main.ino

--- Codes/ESC-main.ino
+++ Codes/ESC-main.ino
@@ -24,16 +24,11 @@ Arduino IDE 2.3.5
 Arduino AVR Boards 1.8.6
 */
 #include <Wire.h>
-#include <PID_AutoTune_v0.h>
+#include <AutoPID.h>
 #include <Adafruit_PCF8574.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS A2
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature temp(&oneWire);
-
-Adafruit_PCF8574 pcf;
 //PCF8574 PNIS
 #define POWERON_ALERT_PIN 0
 #define LOWBAT_ALERT_PIN 1
@@ -41,7 +36,7 @@ Adafruit_PCF8574 pcf;
 #define SENSORLESS_MODE_PIN 4
 #define CONTROL_MODE_PIN 5
 #define DEBUG_MODE_PIN 6
-#define TEST_MODE_PIN 7
+#define REVERSE_MODE_PIN 7
 
 //Arduino PINS
 #define BRAKE_PIN 2
@@ -49,22 +44,35 @@ Adafruit_PCF8574 pcf;
 #define HALL_B_PIN 7
 #define HALL_C_PIN 6
 
-
-#define SPEED_UP A1
-#define SPEED_DOWN A7
+#define SPEED_UP 12
+#define SPEED_DOWN 13
 
 #define R_SHUNT_PIN A0
 #define THROTTLE_PIN A2
 #define TEMP_PIN A3
+#define ONE_WIRE_BUS A3
 #define BAT_VOLTAGE_PIN A6
 
 #define PWM_MAX_DUTY 255
 #define PWM_MIN_DUTY 0
 #define PWM_START_DUTY 100
+#define Kp 4.3
+#define Ki 3.6
+#define Kd 0
 
 volatile byte hall_state = 0;
-byte motor_speed;
-bool SENSORLESS_MODE = false, CONTROL_MODE = false, DEBUG_MODE = false, TEST_MODE = false;
+volatile uint16_t hall_count = 0;
+const byte pole_pairs = 7;  //6, 7, 14 pairs
+unsigned long prev_rpm_time = 0;
+double meas_motor_speed = 0;   //RPM
+double set_motor_speed = 300;  //RPM
+double PWM_DUTY = 0;
+bool SENSORLESS_MODE = false, CONTROL_MODE = false, DEBUG_MODE = false, REVERSE_MODE = false;
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature temp(&oneWire);
+AutoPID myPID(&meas_motor_speed, &set_motor_speed, &PWM_DUTY, PWM_MIN_DUTY, PWM_MAX_DUTY, Kp, Ki, Kd);
+Adafruit_PCF8574 pcf;
 
 void setup() {
   Serial.begin(115200);
@@ -72,7 +80,8 @@ void setup() {
   temp.begin();
   if (!pcf.begin(0x20, &Wire)) {
     Serial.println("PCF8574 ERROR");
-    while (1);
+    while (1)
+      ;
   }
   pcf.pinMode(POWERON_ALERT_PIN, OUTPUT);
   pcf.pinMode(LOWBAT_ALERT_PIN, OUTPUT);
@@ -80,7 +89,7 @@ void setup() {
   pcf.pinMode(SENSORLESS_MODE_PIN, INPUT);
   pcf.pinMode(CONTROL_MODE_PIN, INPUT);
   pcf.pinMode(DEBUG_MODE_PIN, INPUT);
-  pcf.pinMode(TEST_MODE_PIN, INPUT);
+  pcf.pinMode(REVERSE_MODE_PIN, INPUT);
 
   pinMode(BRAKE_PIN, INPUT);
   pinMode(HALL_A_PIN, INPUT);
@@ -116,12 +125,16 @@ void setup() {
   if (pcf.digitalRead(DEBUG_MODE_PIN) == LOW) {
     DEBUG_MODE = true;
   }
-  if (pcf.digitalRead(TEST_MODE_PIN) == LOW) {
-    TEST_MODE = true;
+  if (pcf.digitalRead(REVERSE_MODE_PIN) == LOW) {
+    REVERSE_MODE = true;
   }
 
   SET_PWM_DUTY(PWM_START_DUTY);
-  motor_speed = PWM_START_DUTY;
+  
+  float tol = set_motor_speed * 0.03;   //3% band
+  myPID.atSetPoint(tol);
+  // myPID.setBangBang(4);
+  // myPID.setTimeStep(4000);
 
   sei();          // Enable global interrupts
   UPDATE_HALL();  // Initialize state
@@ -129,26 +142,22 @@ void setup() {
 }
 
 void loop() {
+  GET_MOTOR_SPEED();
+  MEASUREMENT();
   if (CONTROL_MODE) {
-    if (!digitalRead(SPEED_UP) && motor_speed < PWM_MAX_DUTY) {
-      motor_speed++;
-      SET_PWM_DUTY(motor_speed);
-      delay(100);
-    }
-    if (!digitalRead(SPEED_DOWN) && motor_speed > PWM_MIN_DUTY) {
-      motor_speed--;
-      SET_PWM_DUTY(motor_speed);
-      delay(100);
-    }
+    //Constant speed control mode
+    myPID.run();
+    SET_PWM_DUTY(PWM_DUTY);
   } else {
-    motor_speed = map(analogRead(THROTTLE_PIN), 0, 1024, 0, 255);
-    SET_PWM_DUTY(motor_speed);
+    //eScooter control mode
+    PWM_DUTY = map(analogRead(THROTTLE_PIN), 0, 1023, 0, 255);
+    SET_PWM_DUTY(PWM_DUTY);
   }
 
   if (DEBUG_MODE) {
-    Serial.println((float(motor_speed) / 255) * 100);
+    Serial.println((PWM_DUTY / 255) * 100);
     Serial.println(hall_state, BIN);
-    Serial.println(motor_speed);
+    Serial.println(meas_motor_speed);
     delay(500);
   }
 }
@@ -162,24 +171,37 @@ ISR(PCINT2_vect) {
 }
 
 void UPDATE_HALL() {
+  hall_count++;
   uint8_t pinb = PINB;  // Read PINB
   uint8_t pind = PIND;  // Read PIND
 
-  byte brake_on = (pind & 0b00000100) ? 1 : 0;  // Check bit 0 (pin 8)
+  byte brake_on = (pind & 0b00000100) ? 1 : 0;  // Check bit 2 (pin 2)
   if (!brake_on) {
     byte hall_a = (pinb & 0b00000001) ? 1 : 0;  // Check bit 0 (pin 8)
     byte hall_b = (pind & 0b10000000) ? 1 : 0;  // Check bit 7 (pin 7)
     byte hall_c = (pind & 0b01000000) ? 1 : 0;  // Check bit 6 (pin 6)
 
     hall_state = (hall_a << 2) | (hall_b << 1) | hall_c;
-    switch (hall_state) {
-      case 0b100: AH_BL(); break;
-      case 0b101: AH_CL(); break;
-      case 0b001: BH_CL(); break;
-      case 0b011: BH_AL(); break;
-      case 0b010: CH_AL(); break;
-      case 0b110: CH_BL(); break;
-      default: all_off(); break;  // Invalid state
+    if (REVERSE_MODE) {
+      switch (hall_state) {
+        case 0b100: AH_BL(); break;
+        case 0b101: AH_CL(); break;
+        case 0b001: BH_CL(); break;
+        case 0b011: BH_AL(); break;
+        case 0b010: CH_AL(); break;
+        case 0b110: CH_BL(); break;
+        default: all_off(); break;  // Invalid state
+      }
+    } else {
+      switch (hall_state) {
+        case 0b100: CH_BL(); break;
+        case 0b101: CH_AL(); break;
+        case 0b001: BH_AL(); break;
+        case 0b011: BH_CL(); break;
+        case 0b010: AH_CL(); break;
+        case 0b110: AH_BL(); break;
+        default: all_off(); break;  // Invalid state
+      }
     }
   } else {
     all_off();  // Disable while braking
@@ -189,49 +211,49 @@ void UPDATE_HALL() {
 void all_off() {
   TCCR1A = 0;            // Timer1 off
   TCCR2A = 0;            // Timer2 off
-  PORTD &= ~0b00111000;  // Clear PD3, PD4, PD5
+  PORTD &= ~0b00111000;  // Clear D3, D4, D5
 }
 
 void AH_BL() {
-  PORTD &= ~0b00101000;  // Clear PD3 (CL) and PD5 (AL)
-  PORTD |= 0b00010000;   // Set PD4 (BL)
+  PORTD &= ~0b00101000;  // Clear D3 (CL) and D5 (AL)
+  PORTD |= 0b00010000;   // Set D4 (BL)
   TCCR1A = 0;            // Timer1 off
-  TCCR2A = 0b10000001;   // Enable PWM on OC2A PD11 (AH)
+  TCCR2A = 0b10000001;   // Enable PWM on OC2A D11 (AH)
 }
 
 void AH_CL() {
-  PORTD &= ~0b00110000;  // Clear PD4 (BL), PD5 (AL)
-  PORTD |= 0b00001000;   // Set PD3 (CL)
+  PORTD &= ~0b00110000;  // Clear D4 (BL), D5 (AL)
+  PORTD |= 0b00001000;   // Set D3 (CL)
   TCCR1A = 0;            // Timer1 off
-  TCCR2A = 0b10000001;   // Enable PWM on OC2A PD11 (AH)
+  TCCR2A = 0b10000001;   // Enable PWM on OC2A D11 (AH)
 }
 
 void BH_CL() {
-  PORTD &= ~0b00110000;  // Clear PD4 (BL), PD5 (AL)
-  PORTD |= 0b00001000;   // Set PD3 (CL)
+  PORTD &= ~0b00110000;  // Clear D4 (BL), D5 (AL)
+  PORTD |= 0b00001000;   // Set D3 (CL)
   TCCR2A = 0;            // Timer2 off
-  TCCR1A = 0b00100001;   // Enable PWM on OC1B PD10 (BH)
+  TCCR1A = 0b00100001;   // Enable PWM on OC1B D10 (BH)
 }
 
 void BH_AL() {
-  PORTD &= ~0b00011000;  // Clear PD3 (CL), PD4 (BL)
-  PORTD |= 0b00100000;   // Set PD5 (AL)
+  PORTD &= ~0b00011000;  // Clear D3 (CL), D4 (BL)
+  PORTD |= 0b00100000;   // Set D5 (AL)
   TCCR2A = 0;            // Timer2 off
-  TCCR1A = 0b00100001;   // Enable PWM on OC1B PD10 (BH)
+  TCCR1A = 0b00100001;   // Enable PWM on OC1B D10 (BH)
 }
 
 void CH_AL() {
-  PORTD &= ~0b00011000;  // Clear PD3 (CL), PD4 (BL)
-  PORTD |= 0b00100000;   // Set PD5 (AL)
+  PORTD &= ~0b00011000;  // Clear D3 (CL), D4 (BL)
+  PORTD |= 0b00100000;   // Set D5 (AL)
   TCCR2A = 0;            // Timer2 off
-  TCCR1A = 0b10000001;   // Enable PWM on OC1A PD9 (CH)
+  TCCR1A = 0b10000001;   // Enable PWM on OC1A D9 (CH)
 }
 
 void CH_BL() {
-  PORTD &= ~0b00101000;  // Clear PD3 (CL), PD5 (AL)
-  PORTD |= 0b00010000;   // Set PD4 (BL)
+  PORTD &= ~0b00101000;  // Clear D3 (CL), D5 (AL)
+  PORTD |= 0b00010000;   // Set D4 (BL)
   TCCR2A = 0;            // Timer2 off
-  TCCR1A = 0b10000001;   // Enable PWM on OC1A PD9 (CH)
+  TCCR1A = 0b10000001;   // Enable PWM on OC1A D9 (CH)
 }
 
 void SET_PWM_DUTY(byte duty) {
@@ -241,11 +263,21 @@ void SET_PWM_DUTY(byte duty) {
   OCR2A = duty;  // Set PWM duty cycle of PD11 (AH)
 }
 
-unsigned long prev = 0;
+void GET_MOTOR_SPEED() {
+  if (millis() - prev_rpm_time >= 100) {
+    uint16_t count = hall_count;
+    hall_count = 0;
+    float mech_hz = (count * 10.0) / (6.0 * pole_pairs);
+    meas_motor_speed = mech_hz * 60.0;
+    prev_rpm_time = millis();
+  }
+}
+
+unsigned long prev_time = 0;
 void MEASUREMENT() {
   float Vref = 5.0;
   float voltageRatio = 7.0;
-  if (millis() - prev > 1000) {
+  if (millis() - prev_time > 1000) {
     float V_shunt = analogRead(R_SHUNT_PIN) * (Vref / 1024.0);
     float current = V_shunt * (30.0 / 0.075);
 
@@ -253,6 +285,8 @@ void MEASUREMENT() {
     if (V_bat < 48.0) {
       Serial.println("ALERT: LOW BATTERY");
       pcf.digitalWrite(LOWBAT_ALERT_PIN, HIGH);
+    } else {
+      pcf.digitalWrite(LOWBAT_ALERT_PIN, LOW);
     }
 
     temp.requestTemperatures();
@@ -260,6 +294,8 @@ void MEASUREMENT() {
     if (temp_c > 100.0) {
       Serial.println("ALERT: HIGH TEMPERATURE");
       pcf.digitalWrite(HIGHTEMP_ALERT_PIN, HIGH);
+    } else {
+      pcf.digitalWrite(HIGHTEMP_ALERT_PIN, LOW);
     }
 
     if (DEBUG_MODE) {
@@ -270,7 +306,9 @@ void MEASUREMENT() {
       Serial.print(" A\tTemp: ");
       Serial.print(temp_c);
       Serial.println(" C");
+      Serial.print("RPM: ");
+      Serial.println(meas_motor_speed);
     }
-    prev = millis();
+    prev_time = millis();
   }
 }
